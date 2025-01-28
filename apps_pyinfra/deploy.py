6d6ebc8146306a7f51fa63d8c3adb0ef7a7ce334
0va <adasiunas@gmail.com>
2025-01-28 19:49:28 +0200

    install kubectl and git

--- apps_pyinfra/deploy.py
+++ apps_pyinfra/deploy.py
@@ -1,4 +1,4 @@
-from pyinfra.operations import server
+from pyinfra.operations import server, dnf
 
 
 server.shell(
@@ -20,4 +20,19 @@ server.shell(
         "sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64",
         "minikube start"
     ]
+)
+
+server.shell(
+    name="Install kubectl",
+    commands=[
+        'curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"',
+        'curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"',
+        'echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check || exit 1',
+        'sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl'
+    ]
+)
+
+dnf.packages(
+    name="Insall packages: git",
+    packages=["git"]
 )
